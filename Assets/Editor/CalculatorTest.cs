7e3a822e2757736d20e2c33274a1fe157e75837e
Playyer96 <danilo.vanegas@upb.edu.co>
2017-05-11 20:59:41 -0500

    testing automatizado Calculadora

---
+++ Assets/Editor/CalculatorTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+public class CalculatorTest
+{
+    private Calculator calculator = new Calculator(new NumberParser());
+
+    [TestCase("5", "2", false, "7")]
+    [TestCase("122", "658", false, "780")]
+    [TestCase("1312", "MCCCXLVIII", false, "2660")]
+    [TestCase("122", "255", true, "CCCLXXVII")]
+    [TestCase("3589", "23", true, "MMMDCXII")]
+    public void ApplicateSum(string a, string b, bool romanOutpout, string result)
+    {
+        string outpout = calculator.Sum(a, b, romanOutpout);
+
+        Assert.AreEqual(result, outpout);
+    }
+
+    [TestCase("1321", "CXX", true, "MCCI")]
+    [TestCase("82", "76", true, "VI")]
+    [TestCase("14", "22", false, "-8")]
+    [TestCase("CMIX", "102", false, "807")]
+    [TestCase("4", "2", false, "2")]
+    public void AplicateSubstract(string a, string b, bool romanOutpout, string result)
+    {
+        string outpout = calculator.Substract(a, b, romanOutpout);
+
+        Assert.AreEqual(result, outpout);
+    }
+}
---
+++ Assets/Editor/NumberParserTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+public class NumberParserTest 
+{
+    private NumberParser parser = new NumberParser();
+
+    [TestCase(150, "CL")]
+    [TestCase(7, "VII")]
+    [TestCase(9, "IX")]
+    [TestCase(1601, "MDCI")]
+    [TestCase(409, "CDIX")]
+    public void ParsesArabicNumber(int input, string result)
+    {
+        string outpout = parser.ArabicToRoman(input);
+
+        Assert.AreEqual(result, outpout);
+    }
+
+    [TestCase("III", 3)]
+    [TestCase("LXIV", 64)]
+    [TestCase("DLXXI", 571)]
+    [TestCase("MXXIV", 1024)]
+    [TestCase("MMDCXXIII", 2623)]
+    public void ParsesRomanNumber(string input, int result)
+    {
+        int outpout = parser.RomanToArabic(input);
+
+        Assert.AreEqual(result, outpout);
+    }
+}
